94b87bc9f9d940fe448593f53aa5ed16f2930403
Aman88600 <amanbasoya02@gmail.com>
2025-01-28 15:57:48 +0530

    5

---
+++ Internship_prepration/Python_prep/Basic_Topics/5_Basic_DataStructures/dictionaries.py
@@ -0,0 +1,4 @@
+phonebook = {1 : "Aman", 2 : "Nikhil"}
+print(type(phonebook))
+# Key value pairs
+print(phonebook[1]) # Entering the key to get the value associated with it
---
+++ Internship_prepration/Python_prep/Basic_Topics/5_Basic_DataStructures/sets.py
@@ -0,0 +1,3 @@
+numbers = {1,2,3,1}
+print(numbers)
+# Does not allow repetation, does not preserve order
