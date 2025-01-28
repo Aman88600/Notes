a1efbb246f6d8fc5ac7392510281f496041b5d18
Aman88600 <amanbasoya02@gmail.com>
2025-01-28 15:03:23 +0530

    4

---
+++ Internship_prepration/Python_prep/Basic_Topics/4_control_structure/break_continue_and_pass.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    if i == 3:
+        pass # pass does noting
+    if i == 2:
+        continue
+    print(i)
+    if i == 5:
+        break
+    
---
+++ Internship_prepration/Python_prep/Basic_Topics/4_control_structure/conditional_statements.py
@@ -0,0 +1,10 @@
+# if 
+
+a = 14
+b = 12
+if a < b:
+    print("if works")
+elif a > b:
+    print("elif works")
+else:
+    print("All other statements fail")
