49e6fef6dbd73162fbd919c1cb71f0a973914ed4
Aman88600 <amanbasoya02@gmail.com>
2025-06-19 00:54:50 +0530

    'This_is_the_simple_python_code'

---
+++ functions.py
@@ -0,0 +1,8 @@
+# Function is where you can give input get output
+
+def sum(a:float,b:float) -> float:
+    # This is the adding function 
+    '''This is a adding function'''
+    return a + b
+
+print(sum(1,1))
