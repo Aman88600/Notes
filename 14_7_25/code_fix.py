963ce385f941555a2ef2e8edf44ad4638b9d46f4
Aman88600 <amanbasoya02@gmail.com>
2025-07-14 14:24:22 +0530

    'code_fix'

---
+++ 14_7_25/code_fix.py
@@ -0,0 +1,30 @@
+from groq_llm import GroqLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+llm = GroqLLM()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """Decompose the "Content" into clear and simple propositions, ensuring they are interpretable out of context.
+1. Split compound sentences into simple sentences. Maintain the original phrasing from the input whenever possible.
+2. For any named entity that is accompanied by additional descriptive information, separate this information into its own distinct proposition.
+3. Decontextualize the proposition by adding necessary modifier to nouns or entire sentences and replacing pronouns (e.g., "it", "he", "she", "they", "this", "that") with the full name of the entities they refer to.
+4. Present the results as a list of strings, formatted in JSON."""),
+    ("human", "{input}")
+])
+
+text = """ Content:
+The earliest evidence for the Easter Hare (Osterhase) was recorded in south-west Germany in
+1678 by the professor of medicine Georg Franck von Franckenau, but it remained unknown in
+other parts of Germany until the 18th century. Scholar Richard Sermon writes that "hares were
+frequently seen in gardens in spring, and thus may have served as a convenient explanation for the
+origin of the colored eggs hidden there for children. Alternatively, there is a European tradition
+that hares laid eggs, since a hare’s scratch or form and a lapwing’s nest look very similar, and
+both occur on grassland and are first seen in the spring. In the nineteenth century the influence
+of Easter cards, toys, and books was to make the Easter Hare/Rabbit popular throughout Europe.
+German immigrants then exported the custom to Britain and America where it evolved into the
+Easter Bunny. """
+
+runnable = prompt | llm
+output = runnable.invoke({"input": text})
+
+print(output)
